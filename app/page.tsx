35dd8b49047ceb9a62e1c6d9345609bf194717f0
BerkhanTiryakiler <berkhantiryakiler@gmail.com>
2025-03-02 12:20:45 +0300

    yarisma eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import {useRouter} from 'next/navigation'
+
 export default function Home() {
+  const router = useRouter()
+
+
+  
   return (
 
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-6xl my-3">Ana Sayfa</h1>
-      <button className="rounded-lg border py-3 px-5">Yarışmaya Başla</button>
+      <button onClick={()=> router.push('/yarisma')} className="rounded-lg border py-3 px-5">Yarışmaya Başla</button>
 
 
 
--- app/yarisma/page.tsx
+++ app/yarisma/page.tsx
@@ -1,7 +1,61 @@
+"use client";
+
+import { useState } from "react";
+
+
+
+const veri = {
+    soru: "Fransa'nın başkenti neresidir?",
+    secenekler: ["Paris", "Londra", "Berlin", "Madrid"],
+    dogruCevapIndeks: 0
+};
+
+
 export default function Page() {
+
+    const[puan, puanGuncelle] = useState(0);
+    const[can, canGuncelle] = useState(3);
+
+
+    
+
+
+function secimYap(indeks){
+    if(veri.dogruCevapIndeks ===indeks){
+        puanGuncelle(puan + 5);
+    }
+    else{
+        canGuncelle(can - 1);
+        
+    }
+}
+
+
     return (
-        <main className="flex flex-col items-center justify-center h-screen">
-           <h1>Yarışma</h1>
+        <main className="p-8 flex flex-col items-center justify-center h-screen relative">
+            <div id="oyun-bilgi" className="absolute top-4 right-4 flex gap-4">
+           <div>Puan: <span className="border rounded-lg p-3">0</span></div> 
+           <div>Can: <span className="border rounded-lg p-3">3</span></div>
+           </div>
+
+
+
+                <section className = "flex flex-col items-center justify-center">
+                        <p className="mb-8 text-4xl">{veri.soru}</p>
+
+
+                        <div className="flex flex-col items-center justify-center gap-4">
+                        <button onClick={()=>{secimYap(0)}} className="border py-3 px-4 rounded-md">{veri.secenekler[0]}</button>
+                        <button onClick={()=>{secimYap(1)}} className="border py-3 px-4 rounded-md">{veri.secenekler[1]}</button>
+                        <button onClick={()=>{secimYap(2)}} className="border py-3 px-4 rounded-md">{veri.secenekler[2]}</button>
+                        <button onClick={()=>{secimYap(3)}} className="border py-3 px-4 rounded-md">{veri.secenekler[3]}</button>
+
+                        </div>
+                </section>
+
+
+
         </main>
+
     );
 }
